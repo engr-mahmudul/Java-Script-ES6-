8a8dad3e6b129ddd02286d7084b15a4881abfa0e
engr-mahmudul <dev.mahmudulhasan>
2024-12-14 23:22:44 +0200

    Array methods

---
+++ array_method.js
@@ -0,0 +1,33 @@
+const fruits = [ "Banana", "Orange", "Lemon", "Apple", "Mango"]
+
+const result = fruits.find((f)=> f === "Orange")
+// console.log(result)
+const index = fruits.findIndex((f)=> f === "Orange")
+// console.log(index)
+const fitler_fruits = fruits.filter((f)=> f ==="Orange" || f === "Lemon")
+// console.log(fitler_fruits)
+const slice = fruits.slice(1,3)
+// console.log(slice)
+const concat = fruits.concat(["fruit-1", "fruit-2"])
+// console.log(concat)
+const map = fruits.map((f) => f === "Apple")
+// console.log(map)
+
+let numbers = [1,2,3,4,5]
+
+let res = numbers.reduce((total, currentValue)=> total+ currentValue, 0)
+// console.log(res)
+
+// numbers.forEach((f) =>console.log(f))
+
+for (i of numbers){
+    console.log(i)
+}
+ const obj = {
+    a: 5,
+    b:6,
+    c: 7
+ }
+ for (i in obj){
+    console.log(obj[i])
+ }
